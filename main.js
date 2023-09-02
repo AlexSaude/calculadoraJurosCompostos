a689203547562d4413a2368072e69e883b6d7ce0
Alex José <saude.alex2010@gmail.com>
2023-09-02 11:39:44 -0300

    Calculadora Juros Compostos

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const calcularBotao = document.getElementById("calcularBotao");
+    calcularBotao.addEventListener("click", calcular);
+    function calcular(){
+        const taxa = parseFloat(document.getElementById("taxa").value);
+        const tempo = parseFloat(document.getElementById("tempo").value); 
+        const capital = parseFloat(document.getElementById("capital").value); 
+        let resultado;
+        
+        resultado = capital * (taxa + 1) ** tempo;       
+        alert("Resultado: R$" + Math.trunc(resultado) );
+    } 
+}) ;
